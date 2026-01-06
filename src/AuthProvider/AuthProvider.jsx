d6cf76d68d4c2bf4cd545fa0f8a0f5644606ee11
Bushra01-Dhaka <nothingg192@gmail.com>
2026-01-06 20:42:46 +0600

    Created a Custom Hook useAuth to accress AuthContext from anywhere

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 import auth from "../Firebase/firebase.config";
 
 export const AuthContext = createContext();
